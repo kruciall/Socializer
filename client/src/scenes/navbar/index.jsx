fb666222373a45d8ca6c3d64451276229dcf78d0
Vinson Wong <vinson.wong93@gmail.com>
2023-04-14 00:20:31 -0400

    navbar implement complete with mediaquery and dark/light mode available

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -42,7 +42,9 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = "Vinson Wong";
+  // REPLACE MY NAME WITH THIS AFTER BACKEND IS IMPLMENTED
+  // `${user.firstName} ${user.lastName}`
 
   return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
     <FlexBetween gap="1.75rem">
